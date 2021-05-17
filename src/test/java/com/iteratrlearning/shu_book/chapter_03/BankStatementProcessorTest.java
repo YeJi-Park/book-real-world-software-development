ecef3fe943011f8c9f482b18e383f87676855560
yeji <tuilte38@gmail.com>
2021-05-17 21:05:13 +0900

    test: 특정 조건을 만족하는 필터 유닛 테스트 추가

---
+++ src/test/java/com/iteratrlearning/shu_book/chapter_03/BankStatementProcessorTest.java
@@ -0,0 +1,46 @@
+package com.iteratrlearning.shu_book.chapter_03;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.iteratrlearning.shu_book.chapter_02.BankStatementProcessor;
+import com.iteratrlearning.shu_book.chapter_02.BankTransaction;
+
+public class BankStatementProcessorTest {
+	
+	@Test
+	public void shouldFilterTransactionInFeb() {
+		final BankTransaction febrarySalary = 
+				new BankTransaction(LocalDate.of(2019, Month.FEBRUARY, 01), 2_000, "salary");
+		
+		final BankTransaction marchSalary = 
+				new BankTransaction(LocalDate.of(2019, Month.MARCH, 01), 2_000, "salary");
+		
+		final List<BankTransaction> bankTransactions = 
+				List.of(febrarySalary, marchSalary);
+		
+		final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
+		final List<BankTransaction> transactions = 
+				bankStatementProcessor.findTranactions(new BankTransactionIsInFebruaryAndExpensive());
+		
+		assertThat(transactions, contains(febrarySalary));
+		assertThat(transactions, hasSize(1));
+	}
+	
+	public class BankTransactionIsInFebruaryAndExpensive implements BankTransactionFilter {
+
+		@Override
+		public boolean test(BankTransaction bankTransaction) {
+			return bankTransaction.getDate().getMonth() == Month.FEBRUARY &&
+					bankTransaction.getAmount() >= 1_000;
+		}
+
+	}
+}
