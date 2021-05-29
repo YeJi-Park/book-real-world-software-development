e20a2cef5740b2f09dbbe8c870d62a46c043e03d
yeji <tuilte38@gmail.com>
2021-05-29 14:46:16 +0900

    feat: Parser에 문법 파싱을 체크하는 예외 추가

--- src/main/java/com/iteratrlearning/shu_book/chapter_02/BankStatementCSVParser.java
+++ src/main/java/com/iteratrlearning/shu_book/chapter_02/BankStatementCSVParser.java
@@ -5,12 +5,21 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.iteratrlearning.shu_book.chapter_03.CSVSyntaxException;
+
 public class BankStatementCSVParser implements BankStatementParser {
+	
+	private final int EXPECTED_ATTRIBUTES_LENGTH = 3;
+	
 	private static final DateTimeFormatter DATE_PATTERN
 		= DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
 	public BankTransaction parseFrom(final String line) {
 		final String[] cols = line.split(",");
+		
+		if(cols.length < EXPECTED_ATTRIBUTES_LENGTH) {
+			throw new CSVSyntaxException();
+		}
 		
 		final LocalDate date = LocalDate.parse(cols[0], DATE_PATTERN);
 		final double amount = Double.parseDouble(cols[1]);
---
+++ src/main/java/com/iteratrlearning/shu_book/chapter_03/CSVSyntaxException.java
@@ -0,0 +1,11 @@
+package com.iteratrlearning.shu_book.chapter_03;
+
+public class CSVSyntaxException extends RuntimeException {
+	public CSVSyntaxException() {
+		super();
+	}
+	
+	public CSVSyntaxException(String message) {
+		super(message);
+	}
+}
