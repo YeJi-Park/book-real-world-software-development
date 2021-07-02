a7f71902e2ec5589dc0e6cb1c96d07f8131676bc
yeji <tuilte38@gmail.com>
2021-07-03 00:17:42 +0900

    feat: 고객 거래 상태 추가

--- src/main/java/com/iteratrlearning/shu_book/chapter_05/Main.java
+++ src/main/java/com/iteratrlearning/shu_book/chapter_05/Main.java
@@ -8,11 +8,33 @@ public class Main {
 		BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine(env);
 		
 		businessRuleEngine.addAction(facts->{
-			var jobTitle = facts.getFact("jobTitle");
-			if("CEO".equals(jobTitle)) {
-				var name = facts.getFact("name");
-				 Mailer.sendEmail("sales@company.com", "Relevant customer: "+name);
+			var forecastedAmount = 0.0;
+			var dealStage = Stage.valueOf(facts.getFact("stage"));
+			var amount = Double.parseDouble(facts.getFact("amount"));
+			
+			switch(dealStage) {
+			 case LEAD:
+				 forecastedAmount = amount * 0.2;
+				 break;
+			 case EVALUATING:
+				 forecastedAmount = amount * 0.5;
+				 break;
+			 case INTERESTED:
+				 forecastedAmount = amount * 0.8;
+				 break;
+			 case CLOSED:
+				 forecastedAmount = amount;
+				 break;
 			}
+			
+//			var forecastedAmount = amount * switch (dealStage) {
+//			 case LEAD -> 0.2;
+//			 case EVALUATING -> 0.5;
+//			 case INTERESTED -> 0.8;
+//			 case CLOSED -> 1;
+//			}
+			
+			facts.addFact("forecastedAmount", String.valueOf(forecastedAmount));
 		});
 	}
 
